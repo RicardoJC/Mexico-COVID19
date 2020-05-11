5aa58883a62d7179aa2eb4184fb00caf4f611972
Germán Ortiz <jgermanmx@gmail.com>
2020-05-11 16:26:55 -0500

    Se añade feed de twitter para mostrar tweets de síntomas

--- src/SymptomsMonitor.js
+++ src/SymptomsMonitor.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import MexicoCityMap from './MexicoCityMap';
+import TweetEmbed from 'react-tweet-embed';
 
 class SymptomsMonitor extends React.Component{
 
@@ -13,9 +14,14 @@ class SymptomsMonitor extends React.Component{
                 <Container className='mt-5 mb-5'>
                     <div className ='mt-5 mb-5'>
                         <div className='d-flex justify-content-center'>
-                            <h1>Monitor de síntomas</h1>
+                            <h1>Monitor de síntomas</h1> 
                         </div>
-                        <MexicoCityMap/>                     
+                        <MexicoCityMap/>
+                        <h1 className='d-flex justify-content-center'>Tweets sobre síntomas del Covid19</h1>
+                        <div className='d-flex justify-content-center'>
+                            <TweetEmbed id='692527862369357824' options={{cards: 'hidden'}}/>
+                        </div>
+
                     </div>
                 </Container>
                 <Footer/>
