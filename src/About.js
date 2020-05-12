aed060525746d5d8ffa2acb68ff990879946c506
Germán Ortiz <jgermanmx@gmail.com>
2020-05-12 18:37:45 -0500

    Se añade la descripción del equipo de trabajo en 'Acerca de'

--- src/About.js
+++ src/About.js
@@ -14,7 +14,7 @@ class About extends React.Component{
                             <h1>Quiénes somos</h1>
                         </div>
                         <div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum lacinia quis vel eros donec. Lectus quam id leo in vitae turpis massa sed elementum. Tellus pellentesque eu tincidunt tortor aliquam nulla. Dignissim suspendisse in est ante in nibh mauris cursus. Gravida neque convallis a cras semper auctor neque vitae. Posuere ac ut consequat semper. Sit amet commodo nulla facilisi nullam. Semper auctor neque vitae tempus. Mi bibendum neque egestas congue quisque egestas diam in. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Fames ac turpis egestas integer eget aliquet.</p>
+                            <p align='justify'>Somos un grupo de investigadores, profesores y estudiantes pertenecientes a la Universidad Nacional Autónoma de México (UNAM), particularmente al Instituto de Ingeniería (IIUNAM) y al Instituto de Investigaciones en Matemáticas Aplicadas y en Sistemas (IIMAS) con interés en el procesamiento del lenguaje natural. Este proyecto consta de un sistema automático de vigilancia de COVID19 mediante Twitter, en el cual se busca analizar mensajes de esta red social para evaluar el comportamiento de las personas, estados de ánimo, la popularidad de las medidas dadas por el gobierno, y, además, monitorear usuarios con posibles síntomas de coronavirus en tiempo real a través de Internet.</p>
                         </div>
                         <div className='d-flex justify-content-center'>
                             <h2>Colaboradores</h2>
